9b65dd23cbc217c4991b46594d82165cc9db347e
Júlio Cerqueira <julio.cerqueiira@gmail.com>
2023-10-18 14:47:49 -0300

    Create post.ts

---
+++ server/src/models/post.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const postSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+})
+
+export const Post = mongoose.model('Post', postSchema)
