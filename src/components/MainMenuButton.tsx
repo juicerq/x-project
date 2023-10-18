f57c222b99c00200e79a4d69eea34a36c7ed148e
Júlio Cerqueira <julio.cerqueiira@gmail.com>
2023-10-18 14:47:35 -0300

    give props of button to component

--- src/components/MainMenuButton.tsx
+++ src/components/MainMenuButton.tsx
@@ -1,10 +1,17 @@
-type MainMenuButtonType = {
+interface MainMenuButtonType
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string
 }
 
-export default function MainMenuButton({ text }: MainMenuButtonType) {
+export default function MainMenuButton({
+  text,
+  ...buttonProps
+}: MainMenuButtonType) {
   return (
-    <button className=" h-full w-full text-mainPalette-text transition-all lg:hover:bg-mainPalette-textLowlight lg:hover:bg-opacity-50">
+    <button
+      {...buttonProps}
+      className=" h-full w-full text-mainPalette-text transition-all lg:hover:bg-mainPalette-textLowlight lg:hover:bg-opacity-50"
+    >
       <p className="group-active:scale-95">{text}</p>
     </button>
   )
